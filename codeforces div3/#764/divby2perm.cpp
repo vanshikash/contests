ed7d9a7cc2196c946257d557f72672c6e81e8974
Vanshika <vanshikasharma4102@gmail.com>
2022-01-12 16:07:59 +0530

    codeforces 764

---
+++ codeforces div3/#764/divby2perm.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while(t --)
+    {
+       int n;
+       cin>>n;
+       int arr[n];
+       for(int i= 0; i<n;i++)
+       {
+           cin>>arr[i];
+       }
+
+       sort(arr, arr +n);
+
+       for(int i = 0; i< n; i++)
+       {
+           if (arr[i] == i+1)
+           {
+               
+           }
+       }
+    }
+    return 0;
+}
